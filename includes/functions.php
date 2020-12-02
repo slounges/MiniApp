46372ff6ddd1a00f512a25665561d1f9933c580a
Shannon Lounges <shannonlounges@Shannons-MacBook-Pro.local>
2020-12-02 11:30:38 -0500

    Added php stuff

---
+++ includes/functions.php
@@ -0,0 +1,33 @@
+<?php
+    // include the file we just wrote - connect
+     // like a JS import statement
+
+    $result = array();
+
+    function getAllUsers($conn)
+    {
+        $query = "SELECT * FROM ";
+
+        $runQuery = $conn->query($query);
+
+        while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        //return $result;
+        echo(json_encode($result));
+    }
+
+    function getSingleUser($conn, $id)
+    {
+        $query = "SELECT * FROM tbl_favourite_things WHERE id=" . $id . "";
+
+        $runQuery = $conn->query($query);
+
+        while ($row = $runQuery->fetchAll(PDO::FETCH_ASSOC)) {
+            $result[] = $row;
+        }
+
+        //return $result;
+        echo(json_encode($result));
+    }
